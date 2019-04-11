1d3b1d87c0741b2b684a6e1e85bddae1ec388b6b
Sangam Rajpara <sangamrajpara@gmail.com>
2019-04-11 14:18:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const pino = require("express-pino-logger")();
+var cors = require("cors");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 app.use(pino);
 
 app.get("/api/greeting", (req, res) => {
